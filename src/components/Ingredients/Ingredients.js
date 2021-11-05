585f4c06f788c491cff2d8b16958c575146daa3f
Meghan La Breche <Megavictoria@gmail.com>
2021-11-04 19:48:35 -0700

    Filter and Delete

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -8,30 +8,12 @@ const Ingredients = () => {
 	const [userIngredients, setUserIngredients] = useState([]);
 
 	useEffect(() => {
-		fetch(
-			'https://react-hooks-update-24fc4-default-rtdb.firebaseio.com/ingredients.json'
-		)
-			.then((response) => response.json())
-			.then((responseData) => {
-				const loadedIngredients = [];
-				for (const key in responseData) {
-					loadedIngredients.push({
-						id: key,
-						title: responseData[key].title,
-						amount: responseData[key].amount
-					});
-				}
-				setUserIngredients(loadedIngredients);
-			});
-	}, []);
-
-	useEffect(() => {
 		console.log('RENDERING INGREDIENTS', userIngredients);
 	}, [userIngredients]);
 
-	const filteredIngredientsHandler = (filteredIngredients) => {
+	const filteredIngredientsHandler = useCallback((filteredIngredients) => {
 		setUserIngredients(filteredIngredients);
-	};
+	}, []);
 
 	const addIngredientHandler = (ingredient) => {
 		fetch(
@@ -54,9 +36,16 @@ const Ingredients = () => {
 	};
 
 	const removeIngredientHandler = (ingredientId) => {
-		setUserIngredients((prevIngredient) =>
-			prevIngredient.filter((ingredient) => ingredient.id !== ingredientId)
-		);
+		fetch(
+			`https://react-hooks-update-24fc4-default-rtdb.firebaseio.com/ingredients/${ingredientId}.json`,
+			{
+				method: 'DELETE'
+			}
+		).then((response) => {
+			setUserIngredients((prevIngredient) =>
+				prevIngredient.filter((ingredient) => ingredient.id !== ingredientId)
+			);
+		});
 	};
 	return (
 		<div className='App'>
